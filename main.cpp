0d3a589fcc0518c49a937fca50302197aa27eb4d
PeterMagdik <peter.magdik@student.ssnd.sk>
2024-05-15 17:44:59 +0200

    bubble sort implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
 #include <ctime>
 using namespace std;
+
+vector<int> bubbleSort(vector<int> array){
+    for(int i = 0; i < array.size(); i++){
+        for(int j = 0; j < array.size() - 1; j++){
+            if(array[j] > array[j+1]){
+                swap(array[j], array[j+1]);
+            }
+        }
+    }
+    return array;
+}
 
 time_t getTimestamp() {
     return time(NULL);

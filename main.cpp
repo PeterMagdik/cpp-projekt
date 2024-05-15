700750a1f5fa3242ae34a9ed1f71f0fe6826734f
PeterMagdik <peter.magdik@student.ssnd.sk>
2024-05-15 17:49:01 +0200

    selection sort implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
 #include <ctime>
 using namespace std;
+
+vector<int> selectionSort(vector<int> array){
+    int n = array.size();
+    for (int i = 0; i < n - 1; ++i) {
+        int min_index = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (array[j] < array[min_index]) {
+                min_index = j;
+            }
+        }
+        swap(array[i], array[min_index]);
+    }
+    return array;
+}
 
 time_t getTimestamp() {
     return time(NULL);
